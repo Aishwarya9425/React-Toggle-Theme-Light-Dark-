7f85c90db71907329d951081235fbaf43f51c4b1
Aishwarya Janakiraman <aishwarya9425@gmail.com>
2023-01-27 18:53:11 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import { Level1 } from "./components/Level1";
+
+export function App() {
+  return (
+    <div
+      style={{ color: "white", backgroundColor: "#1E1F2C", height: "100vh" }}
+    >
+      <Level1 />
+    </div>
+  );
+}
---
+++ src/components/Level5.jsx
@@ -0,0 +1,7 @@
+export function Level5(props) {
+  return (
+    <>
+      <div>I am level 5</div>
+    </>
+  );
+}
